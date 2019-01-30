f28b9508a04bca45c06246a4aeb312ad45d4342b
Suprasanna Bhaumik <suprasanna.bhaumik86@gmail.com>
2019-01-30 11:04:04 +0530

    Initial commit

---
+++ src/main/java/com/library/user/LibraryManagementBillingQueryApplication.java
@@ -0,0 +1,14 @@
+package com.library.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementBillingQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementBillingQueryApplication.class, args);
+	}
+
+}
+
